34fc49682a6d6f2ccb29c43503e0a75a447a0def
Raphael Trajano <raphaelsrt@gmail.com>
2021-01-15 01:33:44 -0200

    refactor: HomePage component with list of cars content

---
+++ src/app/containers/HomePage/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/macro';
+
+const HomePageWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 1200px;
+  height: auto;
+  justify-content: center;
+  margin: 15px auto auto auto;
+  box-shadow: 10px 10px 5px grey;
+  border: 1px solid #000000;
+  text-align: center;
+  .title {
+    font-family: 'Brush Script MT';
+    font-size: 35px;
+    font-weight: bold;
+  }
+  .list-area {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 2px;
+    justify-content: flex-start;
+    margin-left: 8.5px;
+  }
+`;
+
+export { HomePageWrapper };
